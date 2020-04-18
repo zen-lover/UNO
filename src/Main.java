2dc68c7a2441f6996179fd79a0d22fecbe130e8f
mammad <m.mahdisaeedi99@gmail.com>
2020-04-18 22:37:15 +0430

    version 1.0.0 is ready

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
 import cards.Card;
 import cards.ColorfulCard;
 import cards.NumericalCard;
+import cards.WildDrawFourCard;
 import game.UnoGame;
 
 import java.util.Random;
 
-
+/**
+ * Main class for driven
+ *
+ * @author Mahdi Saeedi
+ * @version 1.0.0
+ * @since 2020-04-18
+ */
 public class Main {
 
-    public static final String RESET = "\033[0m";  // Text Reset
-
-    public static final String BLACK_BOLD = "\033[1;30m";  // BLACK
-    public static final String RED_BOLD = "\033[1;31m";    // RED
-    public static final String GREEN_BOLD = "\033[1;32m";  // GREEN
-    public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String BLUE_BOLD = "\033[1;34m";   // BLUE
-    public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
-    public static final String CYAN_BOLD = "\033[1;36m";   // CYAN
-    public static final String WHITE_BOLD = "\033[1;37m";  // WHITE
-
-
+    /**
+     * Method to drive
+     */
     public static void main(String... args){
+
         UnoGame game = new UnoGame();
         game.init();
         game.start();
